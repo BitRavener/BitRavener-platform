fe36288048e9f925a3ba99076236440ff9b483c1
BitRavener <105620452+BitRavener@users.noreply.github.com>
2022-08-26 17:58:33 +0800

    Create bit.sol

--- contracts/token/bit.sol
+++ contracts/token/bit.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract bit is ERC20{
   
     address public admin;
